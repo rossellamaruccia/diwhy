6ae435c2df2abda384fb3047f6c3979ca2818696
rosellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-07-21 00:02:09 +0200

    azure deploy settings

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const app = express()
 
-require('dotenv').config();
+require("dotenv").config()
 
 // middleware
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('/public')
+app.get("/", (req, res) => {
+  res.send("/public")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
